f58195ef8cbf4457f6a0435c46646184893505a5
Tim Brendel <timbrendelaz@gmail.com>
2015-03-07 20:55:39 -0700

    update product_name and inherits

--- cm.mk
+++ cm.mk
@@ -1,12 +1,12 @@
 $(call inherit-product, device/lge/v410/full_v410.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_NAME := cm_v410
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="v410" \
-    PRODUCT_NAME="e7wifi" \
+    PRODUCT_NAME="e7att" \
     BUILD_FINGERPRINT="lge/e7lte_att_us/e7lte:4.4.2/KOT49I.V41010d/V41010d.1406542706:user/release-keys" \
     PRIVATE_BUILD_DESC="e7lte_att_us-user 4.4.2 KOT49I.V41010d V41010d.1406542706 release-keys"
