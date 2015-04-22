7a217d0e6e794a926865fb3af9f79317e66faa54
Dan Pasanen <dan.pasanen@gmail.com>
2015-04-21 22:28:12 -0500

    overlay: make sure our overlays take precedence over common
    Change-Id: I1a40fb6f3f6c36c6776cea7733faee4bc299e3a1

--- cm.mk
+++ cm.mk
@@ -1,3 +1,6 @@
+# Overlays
+PRODUCT_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
 $(call inherit-product, device/lge/v410/full_v410.mk)
 
 # Inherit some common CM stuff.
@@ -11,6 +14,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="e7att" \
     BUILD_FINGERPRINT="lge/e7lte_att_us/e7lte:4.4.2/KOT49I.V41010d/V41010d.1406542706:user/release-keys" \
     PRIVATE_BUILD_DESC="e7lte_att_us-user 4.4.2 KOT49I.V41010d V41010d.1406542706 release-keys"
-
-# Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
